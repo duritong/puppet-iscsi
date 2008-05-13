1c06be483342b65a6ee69531cd9ef0107960edaa
Marcel Haerry <haerry@puzzle.ch>
2008-05-13 11:22:58 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# iscsi module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "iscsi": }
+class iscsi {}
