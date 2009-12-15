c19b5a38ef1fdcff8c48e7d30c411b03920dad72
Simon Josi <josi@puzzle.ch>
2009-12-15 18:11:04 +0100

    new code style guidelines

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,7 @@
 #
 
 class iscsi {
-    package{iscsi-initiator-utils:
+    package{'iscsi-initiator-utils':
         ensure => present,
         require => [
             File['/lib/udev/getlun.sh'], 
@@ -31,21 +31,21 @@ class iscsi {
         hasstatus => true,
         require => Package[iscsi-initiator-utils],
     }
-    file{"/etc/init.d/iscsi":
+    file{'/etc/init.d/iscsi':
         source => "puppet://$server/iscsi/iscsi.init",
         require => Package[iscsi-initiator-utils],
         before => Service[iscsi],
         owner => root, group => 0, mode => 0755;
     }
-    file{"/lib/udev/getlun.sh":
+    file{'/lib/udev/getlun.sh':
         source => "puppet://$server/iscsi/getlun.sh",
         owner => root, group => 0, mode => 0755;
     }
-    file{"/etc/udev/rules.d/10_persistant_scsi.rules":
+    file{'/etc/udev/rules.d/10_persistant_scsi.rules':
         source => "puppet://$server/iscsi/10_persistant_scsi.rules",
         owner => root, group => 0, mode => 0644;
     }
-    file{"/usr/local/sbin/update_iscsi_database.rb":
+    file{'/usr/local/sbin/update_iscsi_database.rb':
         source => "puppet://$server/iscsi/update_iscsi_database.rb",
         owner => root, group => 0, mode => 0755;
     }
