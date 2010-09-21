929000f22ab6907131b5be76368ade670b4d6cb7
Marcel Haerry <haerry@puzzle.ch>
2010-09-21 16:50:24 +0200

    refactor module to the new standard

--- manifests/connection.pp
+++ manifests/connection.pp
@@ -1,83 +1,83 @@
 define iscsi::connection(
-    $iscsi_initiator_name,
-    $iscsi_initiator_pwd,
-    $iscsi_target_name,
-    $iscsi_target_ip,
-    $iscsi_target_pwd,
-    $iscsi_replacement_timeout,
-    $iscsi_noop_out_interval,
-    $iscsi_noop_out_timeout
+  $iscsi_initiator_name,
+  $iscsi_initiator_pwd,
+  $iscsi_target_name,
+  $iscsi_target_ip,
+  $iscsi_target_pwd,
+  $iscsi_replacement_timeout,
+  $iscsi_noop_out_interval,
+  $iscsi_noop_out_timeout
 ){
-    include iscsi
+  include iscsi
 
-    if ! $iscsi_initiator_name {
-        fail('You must specify $iscsi_initiator_name!')
-    }
-    if ! $iscsi_initiator_pwd {
-        fail('You must specify $iscsi_initiator_pwd!')
-    }
-    if ! $iscsi_target_name {
-        fail('You must specify $iscsi_target_name!')
-    }
-    if ! $iscsi_target_ip {
-        fail('You must specify $iscsi_target_ip!')
-    }
-    if ! $iscsi_target_pwd {
-        fail('You must specify $iscsi_target_pwd!')
-    }
+  if ! $iscsi_initiator_name {
+    fail('You must specify $iscsi_initiator_name!')
+  }
+  if ! $iscsi_initiator_pwd {
+    fail('You must specify $iscsi_initiator_pwd!')
+  }
+  if ! $iscsi_target_name {
+    fail('You must specify $iscsi_target_name!')
+  }
+  if ! $iscsi_target_ip {
+    fail('You must specify $iscsi_target_ip!')
+  }
+  if ! $iscsi_target_pwd {
+    fail('You must specify $iscsi_target_pwd!')
+  }
 
-    # For now, we only implement those parameters we need to, hashes would 
-    # be much more appropriate for such a case, so we wait until they are 
-    # available in puppet. Also, a much cleaner approach would be to move 
-    # those default values into the template. (<%= iscsi_replacement_timeout ||= 120 %>).
+  # For now, we only implement those parameters we need to, hashes would 
+  # be much more appropriate for such a case, so we wait until they are 
+  # available in puppet. Also, a much cleaner approach would be to move 
+  # those default values into the template. (<%= iscsi_replacement_timeout ||= 120 %>).
 
-    if ! $iscsi_replacement_timeout {
-        $iscsi_replacement_timeout = 120
-    }
-    if ! $iscsi_noop_out_interval {
-        $iscsi_noop_out_interval = 5
-    }
-    if ! $iscsi_noop_out_timeout {
-        $iscsi_noop_out_timeout = 5
-    }
+  if ! $iscsi_replacement_timeout {
+    $iscsi_replacement_timeout = 120
+  }
+  if ! $iscsi_noop_out_interval {
+    $iscsi_noop_out_interval = 5
+  }
+  if ! $iscsi_noop_out_timeout {
+    $iscsi_noop_out_timeout = 5
+  }
 
-    file{'/etc/iscsi/initiatorname.iscsi':
-        content => "InitiatorName=$iscsi_initiator_name\nInitiatorAlias=$hostname\n",
-        require => Package['iscsi-initiator-utils'],
-        notify => [
-            Exec['restart_iscsi_daemon_before_discovery'],
-            Exec['discover_iscsi_targets'],
-        ],
-        owner => root, group => 0, mode => 0644;
-    }
-    file{'/etc/iscsi/iscsid.conf':
-        content => template('iscsi/iscsid.conf.erb'),
-        require => Package['iscsi-initiator-utils'],
-        notify => [
-            Exec['update_iscsi_database'],
-            Exec['restart_iscsi_daemon_before_discovery'],
-            Exec['discover_iscsi_targets'],
-        ],
-        owner => root, group => 0, mode => 0600;
-    }
-    exec{'restart_iscsi_daemon_before_discovery':
-        refreshonly => true,
-        before => Exec['discover_iscsi_targets'],
-        command => "/bin/ls /dev/iscsi_* || /etc/init.d/iscsi restart; /bin/true",
-    }
-    exec{'restart_iscsi_daemon_after_discovery':
-        refreshonly => true,
-        require => Exec['discover_iscsi_targets'],
-        command => "/bin/ls /dev/iscsi_* || /etc/init.d/iscsi restart; /bin/true",
-    }
-    exec{'discover_iscsi_targets':
-        refreshonly => true,
-        notify => Exec['restart_iscsi_daemon_after_discovery'],
-        command => "/bin/ls /dev/iscsi_* && /bin/true || /sbin/iscsiadm -m discovery -t sendtargets -p $iscsi_target_ip",
-    }
-    exec{'update_iscsi_database':
-        refreshonly => true,
-        require => File['/usr/local/sbin/update_iscsi_database.rb'],
-        command => "/usr/local/sbin/update_iscsi_database.rb",
-    }
+  file{'/etc/iscsi/initiatorname.iscsi':
+    content => "InitiatorName=$iscsi_initiator_name\nInitiatorAlias=$hostname\n",
+    require => Package['iscsi-initiator-utils'],
+    notify => [
+      Exec['restart_iscsi_daemon_before_discovery'],
+      Exec['discover_iscsi_targets'],
+    ],
+    owner => root, group => 0, mode => 0644;
+  }
+  file{'/etc/iscsi/iscsid.conf':
+    content => template('iscsi/iscsid.conf.erb'),
+    require => Package['iscsi-initiator-utils'],
+    notify => [
+      Exec['update_iscsi_database'],
+      Exec['restart_iscsi_daemon_before_discovery'],
+      Exec['discover_iscsi_targets'],
+    ],
+    owner => root, group => 0, mode => 0600;
+  }
+  exec{'restart_iscsi_daemon_before_discovery':
+    refreshonly => true,
+    before => Exec['discover_iscsi_targets'],
+    command => "/bin/ls /dev/iscsi_* || /etc/init.d/iscsi restart; /bin/true",
+  }
+  exec{'restart_iscsi_daemon_after_discovery':
+    refreshonly => true,
+    require => Exec['discover_iscsi_targets'],
+    command => "/bin/ls /dev/iscsi_* || /etc/init.d/iscsi restart; /bin/true",
+  }
+  exec{'discover_iscsi_targets':
+    refreshonly => true,
+    notify => Exec['restart_iscsi_daemon_after_discovery'],
+    command => "/bin/ls /dev/iscsi_* && /bin/true || /sbin/iscsiadm -m discovery -t sendtargets -p $iscsi_target_ip",
+  }
+  exec{'update_iscsi_database':
+    refreshonly => true,
+    require => File['/usr/local/sbin/update_iscsi_database.rb'],
+    command => "/usr/local/sbin/update_iscsi_database.rb",
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -12,35 +12,35 @@
 #
 
 class iscsi {
-    package{'iscsi-initiator-utils':
-        ensure => present,
-        require => [
-            File['/lib/udev/get_persistant_iscsi_name.sh'], 
-            File['/etc/udev/rules.d/10_persistant_iscsi.rules'], 
-        ],
-    }
-    service{'iscsi':
-        ensure => running,
-        enable => true,
-        hasstatus => true,
-        require => Package['iscsi-initiator-utils'],
-    }
-    service{'iscsid':
-        ensure => running,
-        enable => true,
-        hasstatus => true,
-        require => Package['iscsi-initiator-utils'],
-    }
-    file{'/lib/udev/get_persistant_iscsi_name.sh':
-        source => "puppet://$server/iscsi/get_persistant_iscsi_name.sh",
-        owner => root, group => 0, mode => 0755;
-    }
-    file{'/etc/udev/rules.d/10_persistant_iscsi.rules':
-        source => "puppet://$server/iscsi/10_persistant_iscsi.rules",
-        owner => root, group => 0, mode => 0644;
-    }
-    file{'/usr/local/sbin/update_iscsi_database.rb':
-        source => "puppet://$server/iscsi/update_iscsi_database.rb",
-        owner => root, group => 0, mode => 0755;
-    }
+  package{'iscsi-initiator-utils':
+    ensure => present,
+    require => [
+      File['/lib/udev/get_persistant_iscsi_name.sh'], 
+      File['/etc/udev/rules.d/10_persistant_iscsi.rules'], 
+    ],
+  }
+  service{'iscsi':
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => Package['iscsi-initiator-utils'],
+  }
+  service{'iscsid':
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => Package['iscsi-initiator-utils'],
+  }
+  file{'/lib/udev/get_persistant_iscsi_name.sh':
+    source => "puppet://$server/iscsi/get_persistant_iscsi_name.sh",
+    owner => root, group => 0, mode => 0755;
+  }
+  file{'/etc/udev/rules.d/10_persistant_iscsi.rules':
+    source => "puppet://$server/iscsi/10_persistant_iscsi.rules",
+    owner => root, group => 0, mode => 0644;
+  }
+  file{'/usr/local/sbin/update_iscsi_database.rb':
+    source => "puppet://$server/iscsi/update_iscsi_database.rb",
+    owner => root, group => 0, mode => 0755;
+  }
 }
