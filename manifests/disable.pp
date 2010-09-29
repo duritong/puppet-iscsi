1885a4fbb955bfe28a1559226ff80f0776d12b42
Anselm Strauss <strauss@puzzle.ch>
2010-09-29 14:36:35 +0200

    Added disable class

---
+++ manifests/disable.pp
@@ -0,0 +1,17 @@
+class iscsi::disable {
+  # Don't remove iscsi-initiator-utils package, it is a dependency of xen
+  package{'iscsi-initiator-utils':
+    ensure => present,
+  }
+  service{['iscsi', 'iscsid']:
+    ensure => stopped,
+    enable => false,
+    hasstatus => true,
+    require => Package['iscsi-initiator-utils'],
+  }
+  file{['/lib/udev/get_persistant_iscsi_name.sh',
+        '/etc/udev/rules.d/10_persistant_iscsi.rules',
+        '/usr/local/sbin/update_iscsi_database.rb']:
+    ensure => absent,
+  }
+}
